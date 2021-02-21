604678b7892d6bd899ae8f0f20342262cb211132
enyolka <maczka.emilia@gmail.com>
2021-02-21 19:35:03 +0100

    feat: store and request helper implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+import StoreProvider from "./store/StoreProvider";
+
 import "./App.scss";
 
-const App = () => <header>Hello World!</header>;
+const App = () => {
+  return (
+    <StoreProvider>
+      <header>Hello World!</header>
+      <div>ok</div>
+    </StoreProvider>
+  );
+};
 
 export default App;
