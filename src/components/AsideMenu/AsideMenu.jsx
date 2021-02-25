01b4428bc99e3de6e5e444a95ff1638f588990bc
enyolka <maczka.emilia@gmail.com>
2021-02-25 22:26:44 +0100

    fix: side menu position

--- src/components/AsideMenu/AsideMenu.jsx
+++ src/components/AsideMenu/AsideMenu.jsx
@@ -16,8 +16,10 @@ const AsideMenu = () => {
 
   return (
     <section className={style()}>
-      <UserMenu isUserLogged={Boolean(user)} />
-      {adminMenuComponent}
+      <div className={style("nav-wrapper")}>
+        <UserMenu isUserLogged={Boolean(user)} />
+        {adminMenuComponent}
+      </div>
     </section>
   );
 };
